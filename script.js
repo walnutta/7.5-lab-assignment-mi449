211d2f7b055994e7977d36de6b802e477b0868c5
Nhi Nguyen <129512777+walnutta@users.noreply.github.com>
2026-03-02 15:37:17 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+import {createClient} from 'https://cdn.skypack.dev/@supabase/supabase-js';
+
+const supabaseURL = 'https://uqleuyqswudzewzdrkgs.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxbGV1eXFzd3VkemV3emRya2dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NTU1NTUsImV4cCI6MjA4ODAzMTU1NX0.w680YblnxkAA4cTFVHq3JXqkNp6QcqXIyZ3TT1EujMw'
+const supabase = createClient(supabaseURL, supabaseKey);
+
+let {data:books, error} = await supabase
+    .from('books')
+    .select('*');
+
+if (error) {
+    console.error('Error fetching books:', error);
+} else {
+    books.forEach(book => {
+        const row = document.createElement('tr');
+
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>`;
+    tbody.appendChild(row);
+});
+}

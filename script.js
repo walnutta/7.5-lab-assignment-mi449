1919cb572fde715c08a6194b9dc80e58c2451dd4
Nhi Nguyen <129512777+walnutta@users.noreply.github.com>
2026-03-02 15:42:17 +0000

    fixed error

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const supabaseURL = 'https://uqleuyqswudzewzdrkgs.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxbGV1eXFzd3VkemV3emRya2dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NTU1NTUsImV4cCI6MjA4ODAzMTU1NX0.w680YblnxkAA4cTFVHq3JXqkNp6QcqXIyZ3TT1EujMw'
 const supabase = createClient(supabaseURL, supabaseKey);
 
+
+const tbody = document.getElementById('books');
 let {data:books, error} = await supabase
     .from('books')
     .select('*');
